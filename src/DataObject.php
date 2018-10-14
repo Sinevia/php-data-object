394d033e1894f902d739bcb3d8bdfb42b7df3d9d
Sinevia <info@sinevia.com>
2018-10-14 06:12:39 +0100

    Update DataObject.php

--- src/DataObject.php
+++ src/DataObject.php
@@ -27,14 +27,23 @@ class DataObject {
      */
     function get($name) {
         if (is_string($name) == false) {
-            throw new InvalidArgumentException("The first parameter in the get method in " . get_class($this) . " MUST be of type String: <b>" . gettype($name) . "</b> given");
+            throw new \InvalidArgumentException("The first parameter in the get method in " . get_class($this) . " MUST be of type String: <b>" . gettype($name) . "</b> given");
         }
 
         if (array_key_exists($name, $this->data)) {
             return $this->data[$name];
         } else {
-            throw new OutOfRangeException("There is no $name field in " . get_class($this) . "!");
+            throw new \OutOfRangeException("There is no $name field in " . get_class($this) . "!");
         }
+    }
+    
+    /**
+     * Gets all the data of the instance as associative Array
+     * @param String $name
+     * @return Array
+     */
+    function getAll() {
+        return $this->data;
     }
 
     /**
@@ -45,7 +54,7 @@ class DataObject {
      */
     public function set($name, $value) {
         if (is_string($name) == false) {
-            throw new InvalidArgumentException("The first parameter in the set method class " . get_class($this) . " MUST be of type String: <b>" . gettype($name) . "</b> given");
+            throw new \InvalidArgumentException("The first parameter in the set method class " . get_class($this) . " MUST be of type String: <b>" . gettype($name) . "</b> given");
         }
 
         if (array_key_exists($name, $this->data)) {
@@ -57,6 +66,24 @@ class DataObject {
             $this->data[$name] = $value;
             $this->data_changed[$name] = $value;
         }
+    }
+    
+    public function setAll($data) {
+        if ($this->isAssoc($data) == false)
+            throw new \InvalidArgumentException("The set_all data in class " . get_class($this) . " MUST be an Associative Array: <b>" . gettype($data) . "</b> given");
+
+        foreach ($data as $name => $value) {
+            $this->set($name, $value);
+        }
+    }
+    
+    /**
+     * Taken from PHP manual "is_array" user comments
+     * @param $array
+     * @return Boolean
+     */
+    private function isAssoc($array) {
+        return (is_array($array) && (count($array) == 0 || 0 !== count(array_diff_key($array, array_keys(array_keys($array))))));
     }
 
 }
